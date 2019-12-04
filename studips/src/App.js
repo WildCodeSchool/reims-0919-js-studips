b3464c32529a2ee5c25e7e30481dcee33a3b1995
pablo <pmo83.mp@gmail.com>
2019-12-04 16:01:53 +0100

    5 way installed on server.js

--- studips/src/App.js
+++ studips/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+	return <div className='App'>
+    hello
+  </div>;
 }
 
 export default App;
