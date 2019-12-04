4af079fd0ef655b250a931b187874082cbc1295a
pablo <pmo83.mp@gmail.com>
2019-12-04 16:52:33 +0100

    postCard creat

--- studips/src/App.js
+++ studips/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import postCard from './postCard.jsx'
 
 function App() {
   return (
     <div className="App">
+      <postCard/>
     </div>
   );
 }
---
+++ studips/src/component/postCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+<div className= 'postCard'>
+    yaaaaaaaaaaaaaaa
+</div>
+
+
+
+export default postCard ;
