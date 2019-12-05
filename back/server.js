a44549ddb4ef3ceb2ea180ef948550ce90bde207
Pierre Moulin <phv.moulin@gmail.com>
2019-12-05 14:59:46 +0100

    code cleaned on server.js

--- back/server.js
+++ back/server.js
@@ -3,29 +3,28 @@ const app = express();
 const port = 8000;
 
 app.get('/', (request, response) => {
-  response.send(`Fil d'acctualités`);
+	response.send(`Fil d'acctualités`);
 });
 
 app.get('/jobs', (request, response) => {
-    response.send(`jobs`);
-  });
+	response.send(`jobs`);
+});
 
-  app.get('/accomodations', (request, response) => {
-    response.send(`logements`);
-  });
+app.get('/accomodations', (request, response) => {
+	response.send(`logements`);
+});
 
-  app.get('/cours', (request, response) => {
-    response.send(`cours`);
-  });
+app.get('/cours', (request, response) => {
+	response.send(`cours`);
+});
 
-  app.get('/supplies', (request, response) => {
-    response.send(`équipement scolaire`);
-  });
+app.get('/supplies', (request, response) => {
+	response.send(`équipement scolaire`);
+});
 
-app.listen(port, (err) => {
-  if (err) {
-    throw new Error('Something bad happened...');
-  }
-
-  console.log(`Server is listening on ${port}`);
+app.listen(port, err => {
+	if (err) {
+		throw new Error('Something bad happened...');
+	}
+	console.log(`Server is listening on ${port}`);
 });
