48d6979638c4f092ea43fde49b2e1ca5c77968f6
Pierre Moulin <phv.moulin@gmail.com>
2019-12-05 17:21:33 +0100

    icons imported

--- studips/src/MainThread.js
+++ studips/src/MainThread.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import PostCard from './PostCard';
-import App from './App.css';
+import './App.css';
+import menuIcon from './images/bars-solid.svg';
+import homeIcon from './images/home-solid.svg';
+import searchIcon from './images/search-solid.svg';
+import messageIcon from './images/comments-solid.svg';
+import notifIcon from './images/bell-solid.svg';
+
 
 function MainThread() {
 	return (
 		<div>
 			<div className='topButtons'>
-				<button>...</button>
+				<img
+					src={menuIcon}
+					alt="menu"/>
 				<button>Poster un message</button>
 			</div>
 			<div className='cardList'>
@@ -18,10 +26,18 @@ function MainThread() {
                 <PostCard />
 			</div>
 			<div className='navbar'>
-				<button>Home</button>
-				<button>Recherche</button>
-				<button>Notifications</button>
-				<button>Messagerie</button>
+				<img 
+					src={homeIcon}
+					alt="to home"/>
+				<img
+					src={searchIcon}
+					alt="search"/>
+				<img
+					src={messageIcon}
+					alt="messages"/>	
+				<img
+					src={notifIcon}
+					alt="notifications"/>
 			</div>
 		</div>
 	);
