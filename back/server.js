8f82255ca962da059430d3ccd31623b8cef423a4
Pierre Moulin <phv.moulin@gmail.com>
2019-12-09 15:46:26 +0100

    fixed cors error

--- back/server.js
+++ back/server.js
@@ -89,6 +89,11 @@ const posts = [
       city: "Reims"
   }
 ]
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/posts', (req, res) => {
   res.send(posts)
