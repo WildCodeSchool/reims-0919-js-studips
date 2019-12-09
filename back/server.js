82b4e64c744c75bd9951fac4c07f3ec287330c31
Pierre Moulin <phv.moulin@gmail.com>
2019-12-09 15:26:37 +0100

    delete unnessecary routes

--- back/server.js
+++ back/server.js
@@ -89,24 +89,8 @@ const posts = [
       city: "Reims"
   }
 
-app.get('/', (request, response) => {
+app.get('/posts', (request, response) => {
 	response.send(`Fil d'actualités`);
-});
-
-app.get('/jobs', (request, response) => {
-	response.send(`Jobs`);
-});
-
-app.get('/accomodations', (request, response) => {
-	response.send(`Logements`);
-});
-
-app.get('/courses', (request, response) => {
-	response.send(`Cours`);
-});
-
-app.get('/supplies', (request, response) => {
-	response.send(`Fournitures scolaire`);
 });
 
 app.listen(port, err => {
