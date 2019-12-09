1d7da939cec52d2fa6ca3500d5da7e43353bedf1
Pierre Moulin <phv.moulin@gmail.com>
2019-12-09 15:35:22 +0100

    mainthread turned into class

--- studips/src/MainThread.js
+++ studips/src/MainThread.js
@@ -8,43 +8,56 @@ import messageIcon from './images/comments-solid.svg';
 import notifIcon from './images/bell-solid.svg';
 
 
-function MainThread() {
-	return (
-		<div>
-			<div className='topButtons'>
-				<img
-					className="menuIcon"
-					src={menuIcon}
-					alt="menu"/>
-				<button>Poster un message</button>
+class MainThread extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			posts: []
+		}
+	}
+	componentDidMount() {
+		axios
+			.get("localhost:8000/posts")
+			.then (data => console.log(data))
+	}
+	render() {
+		return (
+			<div>
+				<div className='topButtons'>
+					<img
+						className="menuIcon"
+						src={menuIcon}
+						alt="menu"/>
+					<button>Poster un message</button>
+				</div>
+				<div className='cardList'>
+					<PostCard />
+					<PostCard />
+					<PostCard />
+					<PostCard />
+					<PostCard />
+					<PostCard />
+				</div>
+				<div className='navbar'>
+					<img 
+						className="navbarIcons"
+						src={homeIcon}
+						alt="to home"/>
+					<img
+						className="navbarIcons"
+						src={searchIcon}
+						alt="search"/>
+					<img
+						className="navbarIcons"
+						src={messageIcon}
+						alt="messages"/>	
+					<img
+						className="navbarIcons"
+						src={notifIcon}
+						alt="notifications"/>
+				</div>
 			</div>
-			<div className='cardList'>
-				<PostCard />
-				<PostCard />
-				<PostCard />
-				<PostCard />
-                <PostCard />
-                <PostCard />
-			</div>
-			<div className='navbar'>
-				<img 
-					className="navbarIcons"
-					src={homeIcon}
-					alt="to home"/>
-				<img
-					className="navbarIcons"
-					src={searchIcon}
-					alt="search"/>
-				<img
-					className="navbarIcons"
-					src={messageIcon}
-					alt="messages"/>	
-				<img
-					className="navbarIcons"
-					src={notifIcon}
-					alt="notifications"/>
-			</div>
-		</div>
+	}
 	);
 }
 
