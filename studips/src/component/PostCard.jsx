f0efa920721790a5e608bab46e070533043323f9
Pierre Moulin <phv.moulin@gmail.com>
2019-12-10 10:23:13 +0100

    css update on postcard.jsx

--- studips/src/component/PostCard.jsx
+++ studips/src/component/PostCard.jsx
@@ -5,28 +5,36 @@ import commentIcon from '../images/comments-regular.svg';
 import shareIcon from '../images/share-alt-solid.svg';
 import saveIcon from '../images/download-solid.svg';
 
-function PostCard({postData}) {
+function PostCard({ postData }) {
 	return (
 		<div className='postCard'>
-			<div className = 'entete'>
-				<img className = 'avatar' src = {postData.picture} alt = 'photo de profile'/>	
+			<div className='entete'>
+				<img
+					className='avatar'
+					src={postData.picture}
+					alt='profil'
+				/>
 				<div className='informations'>
-	<p id="name">{postData.firstname} {postData.lastname}</p>
-					<p>{postData.studies}, {postData.city}</p>
-					<p id = 'category'>{postData.category}</p>
+					<p id='name'>
+						{postData.firstname} {postData.lastname}
+					</p>
+					<p>
+						{postData.studies}, {postData.city}
+					</p>
+					<p id='category'>{postData.category}</p>
 				</div>
 			</div>
 			<div className='message'>
 				<p>{postData.content}</p>
 			</div>
 			<div className='actions'>
-				<div className = 'lss'>
-					<img className = 'like' src={heartIcon} alt = 'like'/>
-					<img className = 'search'src= {commentIcon} alt= ' search'/>
-					<img className ='share' src = {shareIcon} alt = 'share' />
+				<div className='postReactions'>
+					<img className='like' src={heartIcon} alt='like' />
+					<img className='comment' src={commentIcon} alt='comment' />
+					<img className='share' src={shareIcon} alt='share' />
 				</div>
-				<div className = 'saveBox'>
-					<img className ='save' src = {saveIcon} alt = 'save' />
+				<div className='saveBox'>
+					<img className='save' src={saveIcon} alt='save' />
 				</div>
 			</div>
 		</div>
