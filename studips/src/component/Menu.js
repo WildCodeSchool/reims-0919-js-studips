6a48fd32b47d64403c53f2b2db2cc604c89dba39
pablo <pmo83.mp@gmail.com>
2019-12-11 17:41:14 +0100

    open close menu finish

--- studips/src/component/Menu.js
+++ studips/src/component/Menu.js
@@ -7,33 +7,35 @@ class Menu extends Component{
 
 		this.state = {
 			showMenu:false,
+			showMenuBoutton:true,
 		}
 		this.showMenu = this.showMenu.bind(this);
 		this.closeMenu = this.closeMenu.bind(this);
+		
 	}
 
 	showMenu (event){
 		event.preventDefault();
 
-		this.setState({showMenu : true}, ()=>{
-			document.addEventListener('click', this.closeMenu);
+		this.setState({showMenu : true , showMenuBoutton: false}, ()=>{
+			document.addEventListener('click', this.closeMenu, this.showMenuBoutton);
 		});
 	} 
 	closeMenu(){
-		this.setState({showMenu: false },()=>{
-			document.removeEventListener('click',this.closeMenu);
+		this.setState({showMenu: false , showMenuBoutton: true},()=>{
+			document.removeEventListener('click',this.closeMenu, this.showMenuBoutton);
 		});
 	}
 	
 	render(){
 		return(
-			<div>
-				<button id = "menuBoutton" onClick={this.showMenu}>
+			<div>{this.state.showMenuBoutton ?(
+				<button id = "menuBoutton" onClick={this.showMenu} >
 					Show menu
-				</button>
+				</button>):(null)}
 				{this.state.showMenu
 				?(
-				<div className = 'menu'>
+				<div className = 'menu' onclick={this.showMenuBoutton}>
 					<div className = 'profile'>
 						<img src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcKVvQJdRW6Kj8X6iO6l5YDn-fU_5ic04qB6udb_fMZhS2qycz&s' alt = 'profil'/>
 						<p>AIMAR , Jean</p>
