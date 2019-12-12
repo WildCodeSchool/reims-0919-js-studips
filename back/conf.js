d74fe854c06a514d0910f53d97a59e5c93e26712
Pierre Moulin <phv.moulin@gmail.com>
2019-12-12 10:04:07 +0100

    removed conf.js from the cache

--- back/conf.js
+++
@@ -1,8 +0,0 @@
-const  mysql = require('mysql');
-const  connection = mysql.createConnection({
-  host :  'localhost', // address of the server
-  user :  'me', // username
-  password :  'secret',
-  database :  'studips',
-});
-module.exports = connection;
