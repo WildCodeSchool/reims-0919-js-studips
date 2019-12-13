584684416928f0e649f73928f00e9ba1f3189380
Pierre Moulin <phv.moulin@gmail.com>
2019-12-13 13:28:12 +0100

    fix postmodal structure

--- studips/src/component/PostModal.jsx
+++ studips/src/component/PostModal.jsx
@@ -6,24 +6,27 @@ function PostModal({ isPostModalVisible }) {
 		<div className='overlay'>
             <div className='newPostCard'>
                 <div className='categoryChoice'>
-                    <p className='categoryName'>Catégorie :</p>
-                    <label className='categoryName' for='choiceEvent'>
-                    <input type='radio' id='choiceEvent' name='category' value='event'/>Evènements</label>
-                    <label className='categoryName' for='choiceAccomodation'>
-                    <input type='radio' id='choiceAccomodation' name='category' value='accomodation' />Logements</label>
-                    <label className='categoryName'for='choiceClass'>
-                    <input type='radio' id='choiceClass' name='category' value='class' />Cours</label>
-                    <label className='categoryName' for='choiceJob'>           
-                    <input  type='radio' id='choiceJob' name='category' value='job' />Jobs / stages</label>  
-                    <label className='categoryName'for='choicesupply'>
-                    <input  type='radio' id='choicesupply' name='category' value='supply' />Fournitures</label>
+                    <p className='subTitle'>Catégorie :</p>
+                    <div className='categoryList'>
+                        <label for='choiceEvent'>
+                        <input type='radio' id='choiceEvent' name='category' value='event'/>Evènements</label>
+                        <label for='choiceAccomodation'>
+                        <input type='radio' id='choiceAccomodation' name='category' value='accomodation' />Logements</label>
+                        <label for='choiceClass'>
+                        <input type='radio' id='choiceClass' name='category' value='class' />Cours</label>
+                        <label for='choiceJob'>           
+                        <input  type='radio' id='choiceJob' name='category' value='job' />Jobs / stages</label>  
+                        <label for='choicesupply'>
+                        <input  type='radio' id='choicesupply' name='category' value='supply' />Fournitures</label>
+                    </div>                    
                 </div>
                 <div className='newPostTitle'>
-                    <p className='categoryName'>Titre :</p>
+                    <p className='subTitle'>Titre :</p>
                     <label for='postTitle'></label>
-                    <textarea id="postTitle"></textarea>
+                    <textarea className='newTitleBox' id="postTitle"></textarea>
                 </div>
                 <div>
+                    <p className='subTitle'>Message :</p>
                     <label for='newPost'></label>
                     <textarea className='newMessageBox' id='newPost'></textarea>
                 </div>
