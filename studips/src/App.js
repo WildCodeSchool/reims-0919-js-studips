04d6e25f41a36bbaede0ee27bf2e5f081397a88f
Valentin JULIEN <valentinjulien93@gmail.com>
2019-12-16 13:33:10 +0100

    form of account creation

--- studips/src/App.js
+++ studips/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import MainThread from './component/MainThread';
 import './App.css';
+import LoginForm from './component/LoginForm.js'
 
 
 function App() {
 	return (
 		<div className='App'>
-			<MainThread />
+			<LoginForm />
 		</div>
 	);
 }
