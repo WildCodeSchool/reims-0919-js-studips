e021e6dd50530b35722b450ad371e38125a0c561
Valentin JULIEN <valentinjulien93@gmail.com>
2020-01-02 14:36:22 +0100

    loginform route

--- studips/src/App.js
+++ studips/src/App.js
@@ -11,6 +11,7 @@ function App() {
 		<div className='App'>
 			<Switch>
 				<Route exact path ='/' component={Welcome} />
+				<Route path='/login' component={LoginForm} />
 				<Route path ='/mainthread' component={MainThread} />
       		</Switch>
 		</div>
