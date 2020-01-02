705ed5347e5f68426f78670ec04470e7a855006a
Valentin JULIEN <valentinjulien93@gmail.com>
2020-01-02 09:57:54 +0100

    router installed

--- studips/src/component/Welcome.js
+++ studips/src/component/Welcome.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import '../App.css'
+import '../App.css';
+import {Link} from 'react-router-dom';
 
 const Welcome = () => (
     <div className = 'welcome'>
         <h1>Bienvenue sur Studips</h1>
         <img src ='https://zupimages.net/up/19/51/iq11.png' ></img>
-        <button>Suivant</button>
+        <Link className= 'bouton' to="/mainthread">Suivant</Link>
     </div>
 )
 
