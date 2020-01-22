cdb367d40b1c114b5cb24acfa6acee0d75b3c376
Pierre Moulin <phv.moulin@gmail.com>
2020-01-22 09:22:14 +0100

    server.js: route get posts saved by user tested and working

--- back/server.js
+++ back/server.js
@@ -98,22 +98,30 @@ app.post('/register', (req, res) => {
   	});
 })
 
+// app.get('/postsaves', verifyToken, (req, res) => {
+// 	const sqlQuery = 'SELECT * from post_saves';
+// 	connection.query(sqlQuery, (err, results) => {
+// 		if (err) {
+// 			console.log(err)
+// 			res.status(500).send("Error getting saved posts");
+// 		} else {
+// 			res.json(results)
+// 		}
+// 	})
+// })
+
 app.get('/postsaves', verifyToken, (req, res) => {
-	const sqlQuery = 'SELECT * from post_saves';
+	const userId = req.authData.sub
+	const sqlQuery = `SELECT post.id, CASE WHEN EXISTS (SELECT * FROM post_saves WHERE post.id=post_saves.post_id AND post_saves.user_id=${userId}) THEN TRUE ELSE FALSE END AS savedByUser FROM post LEFT JOIN post_saves ON post.id=post_saves.post_id GROUP BY post.id`
 	connection.query(sqlQuery, (err, results) => {
 		if (err) {
-			console.log(err)
-			res.status(500).send("Error getting saved posts");
+			console.log(err);
+			res.status(500).send('error getting saved posts from user');
 		} else {
 			res.json(results)
 		}
 	})
 })
-
-// app.get('/:userId/postsaves', verifyToken), (req, res) => {
-// 	const userId = req.authData.sub
-// 	const sqlQuery = 
-// }
 
 app.put('/:userId/postsaves', verifyToken, (req, res) => {
 	const userId = req.authData.sub
