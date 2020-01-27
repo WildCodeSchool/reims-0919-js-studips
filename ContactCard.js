f96b28191c18936699eff137d597eacd9b108a42
Pierre Moulin <phv.moulin@gmail.com>
2020-01-27 13:06:21 +0100

    contactcard.js : create component

---
+++ ContactCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function ContactCard({ contactData, getConversation }) {
+    return (
+        <div 
+            className='contactCard'
+            id={contactData.id}
+            onClick={()=>getConversation(contactData.id)}
+        >
+            <img className='avatar' src={contactData.profile_pic}/>
+            <div className='contactCardUserName'>
+                <p>{contactData.firstname} {contactData.lastname}</p>
+            </div>
+        </div>
+    )
+}
+
+export default ContactCard;
